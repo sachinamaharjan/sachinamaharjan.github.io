1dbdc0a966c1e844bad70c3c78cd70cccce08c16
sachinamaharjan <121913870+sachinamaharjan@users.noreply.github.com>
2024-06-05 16:43:59 +1200

    Delete banner.js

--- banner.js
+++
@@ -1,6 +0,0 @@
-const adWrapper = document.getElementById('adWrapper');
-const studentInfo = document.querySelector('.studentInfo'); // More robust selector
-
-function handleError(image) {
-  // Handle image loading error (e.g., display a placeholder image)
-  image.src = "./assets/placeholder.jpg"; // Replace with your placeholder
